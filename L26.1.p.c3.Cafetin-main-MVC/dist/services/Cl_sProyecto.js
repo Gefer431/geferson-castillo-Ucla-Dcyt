77096d0fd6ea97e8d5bffd0247820c79b71a3593
Geferson Castillo <gefersoncastillo431@gmail.com>
2026-06-09 19:45:25 -0400

    Add files via upload

---
+++ L26.1.p.c3.Cafetin-main-MVC/dist/services/Cl_sProyecto.js
@@ -0,0 +1,5 @@
+import Cl_sMockApi from "./Cl_sMockApi.js";
+export default class Cl_sProyecto extends Cl_sMockApi {
+    static apiUrl = "https://6a210c78b1d0aaf32b4ea915.mockapi.io/cafetin";
+}
+//# sourceMappingURL=Cl_sProyecto.js.map
